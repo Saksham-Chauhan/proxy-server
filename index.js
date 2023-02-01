e91d661d0a73f663a3c5171c530c8c4e8b792779
Saksham <saksham@Sakshams-MacBook-Air.local>
2023-02-01 11:12:04 +0530

    feat:add proxy endpoint

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.options("*", cors());
+
+app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).send("running perfectly");
+});
+
+app.get("/proxy", async (req, res) => {
+  try {
+    const response = await axios({
+      method: req.method,
+      url: req.query.url,
+      data: req.body,
+    });
+    res.set(response.headers);
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(4040, () => {
+  console.log("CORS proxy listening on port 4040");
+});
